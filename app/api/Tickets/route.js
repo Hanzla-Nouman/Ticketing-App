a3785a2f98df9d765aaff9b0ad48ceabe1a91175
Hanzla Nouman <150622496+Hanzla-Nouman@users.noreply.github.com>
2024-06-04 23:43:54 +0500

    Commit

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -1,14 +1,15 @@
-// import { Ticket } from "../../(models)/ticket";
-import { Ticket } from "@/app/(models)/ticket";
+import { Ticket } from "../../(models)/ticket";
+
 import { NextResponse } from "next/server";
 
 export async function POST(req){
     try {
-        console.log("pohnch gyi hai") 
+        console.log("pohnch gyi ha")  
         const body = await req.json()
         console.log("pohnch gyi hai 2") 
         const ticketData = body.formData; 
         console.log("pohnch gyi hai 3") 
+        console.log(process.env.MONGODB_URI)
         await  Ticket.create(ticketData)
         console.log("pohnch gyi hai neechay")
 
